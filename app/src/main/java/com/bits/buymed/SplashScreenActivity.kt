61f52bb546fdc70985ff9eddb2651c9dcaad153d
Anish Mittal <Anish.Mittal@Geminisolutions.com>
2023-09-10 17:36:59 +0530

    First code commit

---
+++ app/src/main/java/com/bits/buymed/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.bits.buymed
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.content.ContextCompat.startActivity
+import android.support.v7.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    // Delay time for SplashScreen in milliseconds
+    private val SPLASH_DELAY: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Using a Handler to delay the transition to MainActivity
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Close the SplashScreenActivity to prevent going back to it
+        }, SPLASH_DELAY)
+    }
+}
